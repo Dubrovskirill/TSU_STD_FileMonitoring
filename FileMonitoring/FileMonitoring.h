4c6f51ebde5ab339a8ae5c0440239f0a843d874d
Dubrovskirill <zet10set11@gmail.com>
2025-02-28 13:05:36 +0700

    FileMonitoring теперь наслденик QObject

--- FileMonitoring/FileMonitoring.h
+++ FileMonitoring/FileMonitoring.h
@@ -1,9 +1,18 @@
 #ifndef FILEMONITORING_H
 #define FILEMONITORING_H
 
+#include <QObject>
+#include <QString>
+#include <QFile>
+#include <QTimer>
+#include <QList>
+#include "IFileObserver.h"
+#include "ILogger.h"
 
-class FileMonitoring
-{
+
+class FileMonitoring: public QObject{
+    Q_OBJECT
+
 public:
     FileMonitoring();
 };
