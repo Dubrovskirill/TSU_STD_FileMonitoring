13f5ef9abe810ca0ec32463242d16bb00eefafad
Dubrovskirill <zet10set11@gmail.com>
2025-03-03 12:48:25 +0700

    Очистка консоли. Исправление для несуществующего файла

--- FileMonitoring/FileMonitoring.h
+++ FileMonitoring/FileMonitoring.h
@@ -5,6 +5,7 @@
 #include <QObject>
 #include <QFile>
 #include <QTimer>
+#include <QProcess>
 #include <iostream>
 #include <thread>
 #include <chrono>
@@ -15,8 +16,10 @@ class FileMonitoring : public QObject {
     Q_OBJECT
 
 public:
+    //accepts a file path and a pointer to the logger object. Initializes variables, creates a timer, and connects it to the slot.
     FileMonitoring(const QString& filePath, ILogger* logger)
         : m_filePath(filePath), m_logger(logger), m_lastSize(-1), m_fileExists(false) {
+        checkFile();
         m_timer = new QTimer(this);
         connect(m_timer, &QTimer::timeout, this, &FileMonitoring::checkFile);
         m_timer->start(100);
@@ -30,16 +33,22 @@ private slots:
         QFile file(m_filePath);
         bool exists = file.exists();
         qint64 size = exists ? file.size() : -1;
+        system("cls");
 
-        if (exists && size != m_lastSize) {
-            m_logger->log(QString("The file exists, the file has been modified. Size: %1 byte").arg(size));
-            emit fileChanged(m_filePath, exists, size);
-        } else if (exists && size == 0) {
-            m_logger->log("The file exists, but it is empty.");
-        } else if (!exists && m_fileExists) {
-            m_logger->log("The file does not exist.");
+
+
+        if (exists) {
+            if (size != m_lastSize) {
+                m_logger->log(QString("The file '%1' exists, the file has been modified. Size: %2 bytes").arg(m_filePath).arg(size));
+            } else if (size == 0) {
+                m_logger->log(QString("The file '%1' exists, but it is empty.").arg(m_filePath));
+            } else {
+                m_logger->log(QString("The file '%1' exists. Size: %2 bytes").arg(m_filePath).arg(size));
+            }
+        } else {
+            // Если файл не существует, выводим сообщение
+            m_logger->log(QString("The %1 file does not exist.").arg(m_filePath));
         }
-
         m_lastSize = size;
         m_fileExists = exists;
     }
